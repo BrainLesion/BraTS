f5897da5f6a999efca122eaa2d793e53d9478dc7
Marcel Rosier <marcel.rosier@gmx.de>
2024-08-06 15:53:39 +0200

    Add africa constants

--- brats/constants.py
+++ brats/constants.py
@@ -41,8 +41,20 @@ class PediatricAlgorithms(Algorithms):
     """BraTS23 Pediatric Segmentation 3rd place (GPU only)"""
 
 
+class BraTSAfricaAlgorithms(Algorithms):
+    """Constants for the available pediatric segmentation algorithms."""
+
+    BraTS23_1 = "BraTS23_1"
+    """BraTS23 BraTS-Africa Segmentation 1st place (GPU only)"""
+    BraTS23_2 = "BraTS23_2"
+    """BraTS23 BraTS-Africa Segmentation 2nd place (GPU only)"""
+    BraTS23_3 = "BraTS23_3"
+    """BraTS23 BraTS-Africa Segmentation 3rd place (GPU only)"""
+
+
 # meta data file paths
 ALGORITHM_DIR = Path(__file__).parent / "algorithms"
 ADULT_GLIOMA_SEGMENTATION_ALGORITHMS = ALGORITHM_DIR / "adult_glioma.yml"
 MENINGIOMA_SEGMENTATION_ALGORITHMS = ALGORITHM_DIR / "meningioma.yml"
 PEDIATRIC_SEGMENTATION_ALGORITHMS = ALGORITHM_DIR / "pediatric.yml"
+BRATS_AFRICA_SEGMENTATION_ALGORITHMS = ALGORITHM_DIR / "africa.yml"
