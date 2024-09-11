6032d7d90c45ee3cab2ebf6beffc179eb81f2245
Marcel Rosier <marcel.rosier@gmx.de>
2024-09-11 16:08:03 +0200

    Change docs theme to furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_theme = "furo"
+# html_static_path = ["_static"]
 
 autodoc_default_options = {
     "members": True,
