8ca2ae17f63597e2dc1fca66f04408f6d23820bc
Marcel Rosier <marcel.rosier@gmx.de>
2024-12-30 14:36:47 +0100

    Adapt default import to split

--- brats/__init__.py
+++ brats/__init__.py
@@ -1,6 +1,7 @@
 from brats.core.inpainting_algorithms import Inpainter
 from brats.core.segmentation_algorithms import (
-    AdultGliomaSegmenter,
+    AdultGliomaPostOpSegmenter,
+    AdultGliomaPreOpSegmenter,
     AfricaSegmenter,
     MeningiomaSegmenter,
     MetastasesSegmenter,
